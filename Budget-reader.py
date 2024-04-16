895b7175d16420c4a49b40e220dd535fd0270948
Torrin <38574537+torrinjbrown@users.noreply.github.com>
2024-04-15 20:49:26 -0500

    Add files via upload

--- Budget-reader.py
+++ Budget-reader.py
@@ -1,10 +1,18 @@
-import csv, xml
+import pandas as pd
+import os 
+import matplotlib.pyplot as plt
+#data = pd.read_csv("Credit.csv")
+data = pd.read_csv("Credit.csv", index_col=0, parse_dates=True)
+
+data.plot()
+plt.show(block=True)
+#Prints the full data of the excel
+
+
+#Prints the just the amount for the credit card 
+#print (data["Amount"])
 
 
 
 
-budget_file = open('Credit.csv')
-budget_reader = csv.reader(budget_file)
-budget_data = list(budget_reader)
-budget_data
 
